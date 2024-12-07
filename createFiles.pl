fcc06dba2c8f8fe758857cf4807ca78d5283f9af
Clément <clement.reniers@etu.univ-lyon1.fr>
2024-12-07 13:52:45 +0100

    created files for every day

---
+++ createFiles.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub touch {
+    my $file = shift;
+    open my $fh, '>', $file or die $!;
+    close $fh;
+}
+
+for my $i (1..25) {
+    if(!(-d "$i")){
+        mkdir "$i";
+        touch "$i/$i.txt";
+        touch "$i/${i}test.txt";
+        touch "$i/$i.pl";
+        touch "$i/$i.1.pl";
+    }
+}
